79766519ff92a82d2d2ee9dd9091eb45194ac01f
graconbease <99304083+graconbease@users.noreply.github.com>
2022-03-23 10:38:59 -0700

    Add files via upload

---
+++ dom-01-starting-code/app.js
@@ -0,0 +1,8 @@
+const h1 = document.getElementById()
+
+//selecting all elements of a tag type
+const headerItemElements = document.getElementsByTagName('h1');
+
+for (const hItemEl of headerItemElements) {
+     console.dir(hItemEl);
+}
